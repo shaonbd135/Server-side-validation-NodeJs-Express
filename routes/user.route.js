457eaabfc99211a489cdac69d25ea36d8409973e
shaonbd135 <shaonsimt@gmail.com>
2023-12-31 00:36:56 +0600

    project updated

--- routes/user.route.js
+++ routes/user.route.js
@@ -3,7 +3,7 @@ const { runValidation } = require("../validation/auth");
 const { registerUser, loginUser } = require("../controllers/user");
 const { userRegistrationValidator, userLoginValidator } = require("../validation/user.auth");
 
-const userRouter = express.Router();
+const userRouter = express.Router(); 
 
 
 userRouter.post("/register", userRegistrationValidator, runValidation, registerUser)
